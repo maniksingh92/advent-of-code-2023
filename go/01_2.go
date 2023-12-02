48a4df97c0c618f07448520b375e3b67497704f2
Manik Singh <maniksingh92@gmail.com>
2023-12-02 01:05:53 -0800

    split code into new puzzle file

---
+++ go/01_2.go
@@ -0,0 +1,27 @@
+package main
+
+var wordSymbols = []string{
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+var digitSymbols = []string{
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+}
