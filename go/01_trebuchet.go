571980cc7200fe2098d98622700590c4ac246fd8
Manik Singh <maniksingh92@gmail.com>
2023-12-01 23:29:29 -0800

    Solve 01 in golang

---
+++ go/01_trebuchet.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+
+	"github.com/maniksingh92/advent-of-code-2023/utils"
+)
+
+func main() {
+	inputs, err := utils.ProcessInputFilePathArg()
+	if err != nil {
+		panic(err)
+	}
+
+	sum := 0
+
+	for _, input := range inputs {
+		// find first digit
+		var firstDigit string
+		for i := 0; i < len(input); i++ {
+			char := input[i]
+			if unicode.IsDigit(rune(char)) {
+				firstDigit = string(char)
+				break
+			}
+		}
+
+		// find last digit
+		var lastDigit string
+		for i := len(input) - 1; i >= 0; i-- {
+			char := input[i]
+			if unicode.IsDigit(rune(char)) {
+				lastDigit = string(char)
+				break
+			}
+		}
+
+		num, err := strconv.Atoi(firstDigit + lastDigit)
+		if err != nil {
+			panic(err)
+		}
+
+		sum += num
+	}
+
+	fmt.Println(sum)
+}
