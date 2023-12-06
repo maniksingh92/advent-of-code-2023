54c46c7c3a90871602d89f7624386ce934dd1796
Manik Singh <maniksingh92@gmail.com>
2023-12-06 07:08:57 -0800

    Solve puzzle 05 day 2

--- python/05.py
+++ python/05.py
@@ -1,11 +1,11 @@
 from collections.abc import Iterator
-from typing import List
+from typing import List, Tuple
 
 def parse_first_line(line: str):
     return list(map(int, line.rstrip().split(": ")[1].split()))
 
 def process_data_maps(lines: List[str]):
-    data_maps: List[List[List[int]]] = []
+    data_maps: List[List[Tuple[int, ...]]] = []
     for line in lines:
         line = line.rstrip()
 
@@ -16,35 +16,55 @@ def process_data_maps(lines: List[str]):
         if line.endswith("map:"):
             continue
 
-        data_maps[-1].append(list(map(int, line.split())))
+        data_maps[-1].append(tuple(map(int, line.split())))
 
     return data_maps
 
 def day_05_puzzle_1(inputs: List[str]):
-    seeds = parse_first_line(inputs[0])
-
+    first_line = parse_first_line(inputs[0])
     data_maps = process_data_maps(inputs[1:])
 
-    locations: List[int] = []
-    for seed in seeds:
-        curr = seed
-        for data_map in data_maps:
-            for data in data_map:
-                destination, source, length = data
+    locations: List[int] = first_line
+    for data_map in data_maps:
+        mapped = []
+        for curr in locations:
+            for destination, source, length in data_map:
                 if source <= curr <= source + length:
-                    curr = destination + curr - source
+                    mapped.append(destination + curr - source)
                     break
-        locations.append(curr)
+            else:
+                mapped.append(curr)
+        locations = mapped
 
     print(min(locations))
 
-def day_05_puzzle_2(inputs: Iterator[str]):
-    first_line = parse_first_line(next(inputs))
+def day_05_puzzle_2(inputs: List[str]):
+    first_line = parse_first_line(inputs[0])
+    data_maps = process_data_maps(inputs[1:])
 
-    seeds: List[List[int]] = []
+    seeds: List[Tuple[int, int]] = []
     for i in range(0, len(first_line), 2):
-        seeds.append([first_line[i],first_line[i+1]])
+        seeds.append((first_line[i],first_line[i] + first_line[i+1]))
+
+    for data_map in data_maps:
+        mapped: List[Tuple[int, int]] = []
+        while len(seeds) > 0:
+            s, e = seeds.pop()
+            for a, b, c in data_map:
+                os = max(b, s)
+                oe = min(b+c, e)
+                if os < oe:
+                    mapped.append((a + os - b, a + oe - b))
+                    if os > s:
+                        seeds.append((s, os))
+                    if oe < e: 
+                        seeds.append((oe, e))
+                    break
+            else:
+                mapped.append((s, e))
+        seeds = mapped
+    print(min(seeds))
 
 if __name__ == "__main__":
     with open("../inputs/05.txt") as inputs:
-        day_05_puzzle_1(inputs.readlines())
+        day_05_puzzle_2(inputs.readlines())
