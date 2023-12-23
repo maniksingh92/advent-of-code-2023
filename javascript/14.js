93bf1cc272004fc8191a13255b6b08d3edc5808b
Manik Singh <maniksingh92@gmail.com>
2023-12-22 17:48:46 -0800

    Solve day 14 part 2

--- javascript/14.js
+++ javascript/14.js
@@ -1,25 +1,70 @@
 import fs from "fs";
 
-import { transpose } from "./util.js";
+import { transpose, rotateClockwise, pipe } from "./util.js";
 
-const data = fs
+const inputs = fs
 	.readFileSync("../inputs/14.txt")
 	.toString()
 	.trim()
 	.split("\n")
 	.map(line => line.split(""));
 
-const grid = transpose(
-	transpose(data)
-		.map(line => line.join("").split("#"))
-		.map(line => line.map(chunk => chunk.split("").toSorted().toReversed().join("")))
-		.map(line => line.join("#"))
-		.map(line => line.split(""))
-);
-
-let sum = 0;
-for (const [i, line] of grid.entries()) {
-	sum += (grid.length - i) * line.filter(ch => ch === "O").length;
+function tiltRocks(data) {
+	return pipe(
+		transpose,
+		x => x.map(line => line.join("").split("#")),
+		x => x.map(line => line.map(chunk => chunk.split("").toSorted().toReversed().join(""))),
+		x => x.map(line => line.join("#")),
+		x => x.map(line => line.split("")),
+		transpose,
+	)(data);
 }
 
-console.log(sum);
+// const grid = tiltRocks(inputs);
+
+// let sum = 0;
+// for (const [i, line] of grid.entries()) {
+// 	sum += (grid.length - i) * line.filter(ch => ch === "O").length;
+// }
+
+// console.log(sum);
+
+function cycle(data) {
+	return pipe(
+		tiltRocks,
+		tiltRocks,
+		tiltRocks,
+		tiltRocks,
+	)(data);
+}
+
+function convertToKey(data) {
+	return data.map(line => line.join("")).join("");
+}
+
+const init = convertToKey(inputs);
+const seen = new Set([init]);
+const arr = [init];
+
+function isSeen(data) {
+	const key = convertToKey(data);
+	if (seen.has(key)) return true;
+
+	seen.add(key);
+	arr.push(key);
+
+	return false;
+}
+
+let grid = inputs;
+let iter = 0;
+while (true) {
+	iter += 1;
+	grid = cycle(grid);
+	if (isSeen(grid)) break;
+}
+
+const first = arr.indexOf(convertToKey(grid));
+const result = arr[(1_000_000_000 - first) % (iter - first) + first];
+
+console.log(result)
--- javascript/util.js
+++ javascript/util.js
@@ -5,3 +5,5 @@ export function transpose(array) {
 export function rotateClockwise(array) {
     return array[0].map((_, index) => array.map(row => row[index]).toReversed())
 }
+
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
